2fd78223013356bec8b3f2956f21042d89f612fb
Stavros Kounis <skounis@gmail.com>
2022-12-18 10:09:22 +0000

    Introduce type checking and more verbose debug messages.

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 def addthis(x,y):
-    print(f"You provided x and y: {x}, {y}")
-    result = x+y
+    print(f"You provided x and y: {x} of type {type(x)}, {y} of type {type(y)}") 
+    try:
+        result = x+y
+    except TypeError as exception:
+        print(f"Wrong type provided: {exception}")
+        result = int(x) + int(y)
     print(f"Which make: {result}")
     return result
 
-print( addthis(1,2))
+print( addthis(1,2) )
+print( addthis(1,"4") )
+print( addthis(1,"two") )
